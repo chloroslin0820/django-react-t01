c0c1cf0cb50b1eca5666a19e05f985a0f28cc1e4
Chloros <chloros@ChlorosdeMacBook-Pro.local>
2024-06-29 22:01:32 +0800

    deploy config

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL
+    ? import.meta.env.VITE_API_URL
+    : "https://efc91d6b-c50e-4973-885f-7efd5d50b7c8-dev.e1-us-east-azure.choreoapis.dev/chloroslindjangoreactt01/backend/v1",
 });
 
 api.interceptors.request.use(
